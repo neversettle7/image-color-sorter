649f9a86430ba8beddc2c8137669fafd2502283a
Giovanni Dini Gentilini <gdinigentilini@gmail.com>
2016-02-11 15:44:26 +0100

    0.0.4 - Dropped conversion functions, output image is not right
    + Dropped custom conversion functions in favor of colorsys native
    functions
    + Output file generated
    - In the output file the colors are wrong (different from the original)

--- pixel.py
+++ pixel.py
@@ -1,52 +1,22 @@
 # The time library is needed to measure execution time
-import time
-start_time = time.time()
-
-# import the PIL library to manipulate images
+# PIL library to manipulate images
 # colorsys library to manipulate colors
 # operator library to sort the values of the array in the fastest way
 from PIL import Image
 import colorsys
-from operator import itemgetter
+import time
+start_time = time.time()
 
-x = 2
+# Temporary variable until height iteration is developed
 y = 0
-value = 100
-
-
-# This function gets an hexrgb value and converts it to an hsv value
-# so it can be sorted in an easier way
-def get_hsv(hexrgb):
-    hexrgb = hexrgb.lstrip("#")  # in case you have Web color specs
-    r, g, b = (int(hexrgb[i:i + 2], 16) / 255.0 for i in range(0, 5, 2))
-    return colorsys.rgb_to_hsv(r, g, b)
-
-
-# This function converts hex colors values to rgb
-def hex_to_rgb(value):
-    value = value.lstrip('#')
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-
-# This function converts rgb colors values to hex
-def rgb_to_hex(rgb):
-    return '#%02x%02x%02x' % rgb
-
-
-# Functions usage examples
-# hex_to_rgb("#ffffff")             #==> (255, 255, 255)
-# hex_to_rgb("#ffffffffffff")       #==> (65535, 65535, 65535)
-# rgb_to_hex((255, 255, 255))       #==> '#ffffff'
-# rgb_to_hex((65535, 65535, 65535)) #==> '#ffffffffffff'
 
 # We open the image and load it
-im = Image.open("imghor.jpg")
-pix = im.load()
+oldimg = Image.open("imghor.jpg")
+pix = oldimg.load()
 
 # We get the width and height of the image for iteration
-width, height = im.size
-size = im.size
+width, height = oldimg.size
+size = oldimg.size
 # To print the tuple (which is not printable as string) we need to add a comma after the variable name
 print("\n-- Size of the image: %s --\n" % (size,))
 
@@ -55,22 +25,39 @@ outpix = []
 
 # Let's cycle through the pixels in the image
 for x in range(0, width):
-    print("Values for pixel of x: %i and y: %i" % (x+1, y+1))
-    print(pix[x, y]) # we get the rgba value for debug purposes
-    # Converts the rgb value to hex
-    hexpix = rgb_to_hex(pix[x, y])
-    print(hexpix) # debug
-    # Converts the hex to hsv
-    hsvpix = get_hsv(hexpix)
-    print(hsvpix)
-    # We append the values to the array
-    outpix.append(hsvpix)
-    print(outpix)
+    print("Values for pixel of x: {0} and y: {1}" .format(x+1, y+1))
+    print("RGB values: {0}" .format(pix[x, y])) # we get the rgb value for debug purposes
+    red, green, blue = (pix[x, y])
+    hsvvalues = colorsys.rgb_to_hsv(red, green, blue)
+    print("HSV values: {0}" .format(hsvvalues))
+    outpix.append(hsvvalues)
     print("\n")
     pass
 
 # We sort the array
 outpix.sort()
-print("-- Sorted array: %s" % (outpix,) + " --")
+print("-- Sorted array: %s" % (outpix,) + " --\n")
+
+# Time to create the new image!
+newimg = Image.new("RGB", (width, height))
+newpix = newimg.load()
+
+# Let's cycle through the image and write the new values
+for x in range(0, width):
+    # Write the new color
+    '''print("outpix: ")
+    print(outpix[x])'''
+    hue, saturation, value = outpix[x]
+    #print("hue: {0}, sat: {1}, value: {2}\n" .format(hue, saturation, value))
+    rgbvalues = colorsys.hsv_to_rgb(hue, saturation, value)
+    #print("rgbvalues: ")
+    #print(rgbvalues)
+    red, green, blue = rgbvalues
+    newpix[x,y] = int(red), int(green), int(blue)
+    print(newpix[x, y])
+    pass
+
+# Save the image
+newimg.save("newimg.jpg")
 
 print("\n-- exec time: %s seconds --" % (time.time() - start_time))
