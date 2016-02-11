8d8f23b7a443b7c49798ddd1522917d157ec04be
Giovanni Dini Gentilini <gdinigentilini@gmail.com>
2016-02-11 19:12:30 +0100

    0.1 - Now working, but the output is really dishomogeneous. Need a a different sorting algorithm
    + The sorting is now working as expected (bug in jpg compression, now
    fixed)
    + New image to run tests
    + Commented out all the prints to reduce exec time on big images
    - The algorithm is not ideal, the color are very sparse

--- pixel.py
+++ pixel.py
@@ -8,12 +8,8 @@ import time
 
 start_time = time.time()
 
-# Temporary variable until height iteration is developed
-y = 0
-
-
 def imgopen(filename):
-    'This function opens an image file'
+    """This function opens an image file"""
     img = Image.open(filename)
     print(img)
     loadedimg = img.load()
@@ -21,79 +17,101 @@ def imgopen(filename):
 
 
 def imgcreate(width, height):
-    'This function creates a new image file - IT IS NOT SAVING IT'
+    """This function creates a new image file - IT IS NOT SAVING IT"""
     img = Image.new("RGB", (width, height))
     newloaded = img.load()
     return (img, newloaded)
 
 
 def saveimg(image, filename):
-    'This function saves the image file on the disk'
-    image.save(filename)
+    """This function saves the image file on the disk
+
+    VERY IMPORTANT : do NOT disable delete the subsampling = 0 setting,
+    otherwise any outputted jpg file will be red-shifted
+
+    """
+    image.save(filename, subsampling=0)
     return
 
 
 def getsize(image):
-    'This function gets the size of the image'
+    """This function gets the size of the image"""
     width, height = image.size
     size = image.size
-    print("\n-- Size of the image is: {0} ({1}x{2})".format(size, width, height))
+    print("\n-- Size of the image is: {0} ({1}x{2})\n".format(size, width, height))
     return (size, width, height)
 
 
 def getpixels(image, width, height):
-    'This function gets the pixels of the image and stores them in an array'
+    """This function gets the pixels of the image and stores them in an array"""
     array = []
-    for x in range(0, width):
-        print("Values for pixel of x: {0} and y: {1}".format(x + 1, y + 1))
-        print("RGB values: {0}".format(image[x, y]))  # we get the rgb value for debug purposes
-        red, green, blue = (image[x, y])
-        hsvcolors = colorsys.rgb_to_hsv(red, green, blue)
-        print("HSV values: {0}".format(hsvcolors))
-        array.append(hsvcolors)
-        print("\n")
-        pass
-    return (array)
+    """print("--------------------------\n"
+          "----- ORIGINAL IMAGE -----\n"
+          "--------------------------\n")"""
+    for y in range(0, height):
+        for x in range(0, width):
+            red, green, blue = (image[x, y])
+            hsvcolors = colorsys.rgb_to_hsv(red, green, blue)
+            array.append(hsvcolors)
+            """print("Values for pixel of x: {0} and y: {1}".format(x + 1, y + 1))
+            print("RGB values: {0}".format(image[x, y]))  # we get the rgb value for debug purposes
+            print("HSV values: {0}".format(hsvcolors))
+            print("\n")"""
+            pass
+    pass
+    return array
 
 
 def sortarray(array):
-    'This function just sorts the array'
+    """This function just sorts the array
+    """
     array.sort()
     return (array)
 
 
 def writepixels(array, image, width, height):
-    'This function writes the pixel values in the output image'
-    for x in range(0, width):
-        # Write the new color
-        hue, saturation, value = array[x]
-        # print("hue: {0}, sat: {1}, value: {2}\n" .format(hue, saturation, value))
-        rgbvalues = colorsys.hsv_to_rgb(hue, saturation, value)
-        # print("rgbvalues: ")
-        # print(rgbvalues)
-        red, green, blue = rgbvalues
-        image[x, y] = int(red), int(green), int(blue)
-        print(image[x, y])
+    """This function writes the pixel values in the output image"""
+    """print("------------------------\n"
+          "----- EDITED IMAGE -----\n"
+          "------------------------\n")"""
+    i = 0
+    for y in range(0, height):
+        for x in range(0, width):
+            # Write the new color
+            hue, saturation, value = array[i]
+            rgbvalues = colorsys.hsv_to_rgb(hue, saturation, value)
+            red, green, blue = rgbvalues
+            image[x, y] = int(red), int(green), int(blue)
+            """print("Values for pixel of x: {0} and y: {1}".format(x + 1, y + 1))
+            print("RGB values: {0}".format(image[x, y]))  # we get the rgb value for debug purposes
+            print("HSV values: {0}".format(array[i]))
+            print("\n")"""
+            i += 1
+            #print(image[x, y])
         pass
+    pass
     return (image)
 
 
-oldimg, oldimgcontent = imgopen("imghor.jpg")
+oldimg, oldimgcontent = imgopen("oldimg.jpg")
 size = getsize(oldimg)
 width = size[1]
 height = size[2]
 
-# Creates an array to store the values
-pixvalues = []
-
+# We store the pixel values of the original image in an array
 pixvalues = getpixels(oldimgcontent, width, height)
 
 sortedvalues = sortarray(pixvalues)
+
+#print(pixvalues)
+#print(sortedvalues)
 
 newimg, newimgcontent = imgcreate(width, height)
 
 sortedimg = writepixels(sortedvalues, newimgcontent, width, height)
 
-saveimg(newimg,"newimg.jpg")
+# Pixel values are correct, but there's an error on the save process
+# because the output image is not right
+saveimg(newimg, "newimg.jpg")
 
 print("\n-- exec time: %s seconds --" % (time.time() - start_time))
