e3f7d7dabd9627729b99ef4ff1ed4c6cb96afa01
Giovanni Dini Gentilini <gdinigentilini@gmail.com>
2016-02-11 13:49:04 +0100

    0.0.3 - Added exec time, basic reading of input image values and sorting
    + Exec time
    + Reads values in first pixel row
    + Adds values to array
    + Array values sorted

--- pixel.py
+++ pixel.py
@@ -1,6 +1,13 @@
-# Importiamo la libreria PIL
+# The time library is needed to measure execution time
+import time
+start_time = time.time()
+
+# import the PIL library to manipulate images
+# colorsys library to manipulate colors
+# operator library to sort the values of the array in the fastest way
 from PIL import Image
 import colorsys
+from operator import itemgetter
 
 x = 2
 y = 0
@@ -10,8 +17,8 @@ value = 100
 # This function gets an hexrgb value and converts it to an hsv value
 # so it can be sorted in an easier way
 def get_hsv(hexrgb):
-    hexrgb = hexrgb.lstrip("#")   # in case you have Web color specs
-    r, g, b = (int(hexrgb[i:i+2], 16) / 255.0 for i in range(0,5,2))
+    hexrgb = hexrgb.lstrip("#")  # in case you have Web color specs
+    r, g, b = (int(hexrgb[i:i + 2], 16) / 255.0 for i in range(0, 5, 2))
     return colorsys.rgb_to_hsv(r, g, b)
 
 
@@ -26,24 +33,44 @@ def hex_to_rgb(value):
 def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
 
-'''
-# Functions usage examples
-hex_to_rgb("#ffffff")             #==> (255, 255, 255)
-hex_to_rgb("#ffffffffffff")       #==> (65535, 65535, 65535)
-rgb_to_hex((255, 255, 255))       #==> '#ffffff'
-rgb_to_hex((65535, 65535, 65535)) #==> '#ffffffffffff'
-'''
 
-# Can be many different formats.
+# Functions usage examples
+# hex_to_rgb("#ffffff")             #==> (255, 255, 255)
+# hex_to_rgb("#ffffffffffff")       #==> (65535, 65535, 65535)
+# rgb_to_hex((255, 255, 255))       #==> '#ffffff'
+# rgb_to_hex((65535, 65535, 65535)) #==> '#ffffffffffff'
+
+# We open the image and load it
 im = Image.open("imghor.jpg")
 pix = im.load()
-# Get the width and hight of the image for iterating over
-widht, height = im.size
+
+# We get the width and height of the image for iteration
+width, height = im.size
 size = im.size
-print(size)
-# Get the RGBA Value of the a pixel of an image
-hexpix = rgb_to_hex(pix[x, y])
-print(pix[x, y])
-print(hexpix)
-hsvpix = get_hsv(hexpix)
-print(hsvpix)
+# To print the tuple (which is not printable as string) we need to add a comma after the variable name
+print("\n-- Size of the image: %s --\n" % (size,))
+
+# Creates an array to store the values
+outpix = []
+
+# Let's cycle through the pixels in the image
+for x in range(0, width):
+    print("Values for pixel of x: %i and y: %i" % (x+1, y+1))
+    print(pix[x, y]) # we get the rgba value for debug purposes
+    # Converts the rgb value to hex
+    hexpix = rgb_to_hex(pix[x, y])
+    print(hexpix) # debug
+    # Converts the hex to hsv
+    hsvpix = get_hsv(hexpix)
+    print(hsvpix)
+    # We append the values to the array
+    outpix.append(hsvpix)
+    print(outpix)
+    print("\n")
+    pass
+
+# We sort the array
+outpix.sort()
+print("-- Sorted array: %s" % (outpix,) + " --")
+
+print("\n-- exec time: %s seconds --" % (time.time() - start_time))
